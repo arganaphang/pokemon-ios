05dbe6c339c6f81f20d31994e51bd13f504182d2
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-08-20 12:56:48 +0700

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Argana Phangquestian on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
